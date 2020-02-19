f92f1d1f7d8aac2f844241ee267607e7a83c5d76
Ying Wang <yingw787.personal@gmail.com>
2020-02-19 16:37:49 -0500

    Update src/index.js update path to App.jsx.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import App from './App/App.jsx';
+import App from './core/App/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
