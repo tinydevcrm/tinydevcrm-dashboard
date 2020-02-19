6b5a45572790e3b8cacc119b071b9c849a5510c8
Ying Wang <yingw787.personal@gmail.com>
2020-02-19 18:59:56 -0500

    Update SignupForm.jsx SCSS modules to namespace classes.

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -2,11 +2,11 @@
 
 import React from 'react';
 
-import { btn } from './SignupForm.module.scss';
+import styles from './SignupForm.module.scss';
 
 
 const SignupForm = () => (
-    <h2 className={btn}>Hi</h2>
+    <h2 className={styles.btn}>Hi</h2>
 )
 
 export default SignupForm;
