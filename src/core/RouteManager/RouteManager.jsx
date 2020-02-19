0221f3c2a8ed6fa4edb2a0ee46e3e93f79b3e899
Ying Wang <yingw787.personal@gmail.com>
2020-02-19 16:37:09 -0500

    Move src/App to src/core/App, and add src/core/RouteManager.

---
+++ src/core/RouteManager/RouteManager.jsx
@@ -0,0 +1,9 @@
+// This component manages all client-side routing for 'tinydevcrm-dashboard'.
+//
+// I'm not sure whether this is necessarily the same as <App />. If it is, then
+// this component duplicates functionality. If it doesn't, then this component
+// successfully serves to separate concerns.
+//
+// According to this blog post
+// (https://blog.pshrmn.com/simple-react-router-v4-tutorial/), current "best
+// practice" is to separate out <App /> and <RouteManager /> concerns.
