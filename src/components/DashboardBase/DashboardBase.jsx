66876d0cdfe267694764675a7a1ccdd1e960ebf0
Ying Wang <yingw787.personal@gmail.com>
2020-02-20 18:38:14 -0500

    Add empty DashboardBase React component.

---
+++ src/components/DashboardBase/DashboardBase.jsx
@@ -0,0 +1,11 @@
+// Base dashboard layout (sidebar, navbar, empty content, etc.)
+
+import React from 'react';
+
+import styles from '_DashboardBase.module.scss';
+
+const DashboardBase = () => (
+    <h1>hi</h1>
+);
+
+export default DashboardBase;
