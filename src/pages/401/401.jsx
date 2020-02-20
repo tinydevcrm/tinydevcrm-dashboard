69d956b9c265cbaee7d18c5c98fd38b3acca8a32
Ying Wang <yingw787.personal@gmail.com>
2020-02-20 13:25:45 -0500

    Update 401.jsx adding basic styling and component rendering, and rename 401.module.scss to fourZeroOne.module.scss to fix SCSS module rendering issues and check imports of solarized.scss.

---
+++ src/pages/401/401.jsx
@@ -0,0 +1,14 @@
+// HTTP 401 (Unauthorized) page.
+
+import React from 'react';
+
+import styles from './fourZeroOne.module.scss';
+
+
+const FourZeroOne = () => (
+    <div className={styles.wrap}>
+        <h1 className={styles.wrap}>hi</h1>
+    </div>
+);
+
+export default FourZeroOne;
