d52c73237e7e5d762795a967d8ee6e39d0cab039
Ying Wang <yingw787.personal@gmail.com>
2020-02-20 20:43:14 -0500

    Update _401.jsx have columns with responsive sizes and responsive offsets.

--- src/pages/401/_401.jsx
+++ src/pages/401/_401.jsx
@@ -5,8 +5,11 @@ import {
     Row,
     Col
 } from 'antd';
+import classnames from 'classnames';
 
+import 'antd/dist/antd.css';
 import styles from './_401.module.scss';
+
 
 const DemoBox = (props) => (
     <p className={`height-${props.value}`}>
@@ -14,15 +17,33 @@ const DemoBox = (props) => (
     </p>
 );
 
+// There's no way to add mobile responsiveness with offset, even though the
+// direct classes exist. If you add an offset with the offset prop, the offset
+// will remain constant throughout different mobile experiences. Therefore,
+// combine classes for React.js components using the 'classnames' package.
+// Relevant Stack Overflow post: https://stackoverflow.com/a/38383564/1497211
+const columnWithResponsiveOffsets = classnames(
+    styles.col,
+    'ant-col-xs-offset-2',
+    'ant-col-sm-offset-4',
+    'ant-col-md-offset-5',
+    'ant-col-lg-offset-6',
+    'ant-col-xl-offset-7',
+    'ant-col-xxl-offset-8'
+);
 
 const _401 = () => (
     <div className={styles.container}>
-        <p className={styles.tag}>Align Center</p>
-        <Row type="flex" justify="space-around" align="middle" className={styles.row}>
-            <Col span={4} className={styles.col}>
-                <DemoBox value={100}>
-                    col-4
-                </DemoBox>
+        <Row type="flex" align="middle">
+            <Col
+                xs={20}
+                sm={16}
+                md={14}
+                lg={12}
+                xl={10}
+                xxl={8}
+                className={columnWithResponsiveOffsets}>
+                col-6 with custom responsive offsets
             </Col>
         </Row>
     </div>
