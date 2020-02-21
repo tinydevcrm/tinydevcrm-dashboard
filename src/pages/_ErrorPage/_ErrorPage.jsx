265dfb8dc134598bf0643f4c146bbd00a861e26d
Ying Wang <yingw787.personal@gmail.com>
2020-02-21 14:44:14 -0500

    Add src/pages/_ErrorPage for generic HTTP error pages.

---
+++ src/pages/_ErrorPage/_ErrorPage.jsx
@@ -0,0 +1,38 @@
+// Generic error page (mostly for HTTP errors).
+//
+// Props include:
+// - errorCode
+// - primaryText
+// - secondaryText
+
+import React from 'react';
+import {
+    Button,
+    Icon
+} from 'antd';
+
+import PageCenterWrapper from 'components/PageCenterWrapper/PageCenterWrapper.jsx';
+
+import 'antd/dist/antd.css';
+import styles from './_ErrorPage.module.scss';
+
+
+const _ErrorPage = (props) => (
+    <PageCenterWrapper>
+        <div className={styles.errorCode}>
+            {props.errorCode}
+        </div>
+        <div className={styles.primaryText}>
+            {props.primaryText}
+        </div>
+        <div className={styles.secondaryText}>
+            {props.secondaryText}
+        </div>
+        <Button type="primary" size={"large"}>
+            <Icon type="home" />
+            Home
+        </Button>
+    </PageCenterWrapper>
+);
+
+export default _ErrorPage;
