459341cc08f87d657ab331e9793663606fe386c7
Ying Wang <yingw787.personal@gmail.com>
2020-02-21 14:45:02 -0500

    Delete _401.module.scss remove styling differences between HTTP error pages, and update _401.jsx use refactored page component.

--- src/pages/401/_401.jsx
+++ src/pages/401/_401.jsx
@@ -1,60 +1,15 @@
 // HTTP 401 (Unauthorized) page.
 
 import React from 'react';
-import {
-    Row,
-    Col,
-    Button,
-    Icon
-} from 'antd';
-import classnames from 'classnames';
 
-import 'antd/dist/antd.css';
-import styles from './_401.module.scss';
+import _ErrorPage from 'pages/_ErrorPage/_ErrorPage.jsx';
 
-
-// There's no way to add mobile responsiveness with offset, even though the
-// direct classes exist. If you add an offset with the offset prop, the offset
-// will remain constant throughout different mobile experiences. Therefore,
-// combine classes for React.js components using the 'classnames' package.
-// Relevant Stack Overflow post: https://stackoverflow.com/a/38383564/1497211
-const columnWithResponsiveOffsets = classnames(
-    styles.col,
-    'ant-col-xs-offset-2',
-    'ant-col-sm-offset-4',
-    'ant-col-md-offset-5',
-    'ant-col-lg-offset-6',
-    'ant-col-xl-offset-7',
-    'ant-col-xxl-offset-8'
-);
 
 const _401 = () => (
-    <div className={styles.container}>
-        <Row>
-            <Col
-                xs={20}
-                sm={16}
-                md={14}
-                lg={12}
-                xl={10}
-                xxl={8}
-                className={columnWithResponsiveOffsets}>
-                <div className={styles.errorCode}>
-                    401
-                </div>
-                <div className={styles.primaryText}>
-                    Oops.. You just found an error page..
-                </div>
-                <div className={styles.secondaryText}>
-                    We are sorry but our service is currently not available...
-                </div>
-                <Button type="primary" size={"large"}>
-                    <Icon type="home" />
-                    Home
-                </Button>
-            </Col>
-        </Row>
-    </div>
+    <_ErrorPage
+        errorCode={'401'}
+        primaryText={'Oops.. You just found an error page..'}
+        secondaryText={'We are sorry but our service is currently not available..'} />
 );
 
 export default _401;
