50b510dc2343f22b8d9f1d342382d176926cf07c
Ying Wang <yingw787.personal@gmail.com>
2020-02-20 19:08:46 -0500

    Update DashboardBase.jsx add base dashboard from ant design tutorial.

--- src/components/DashboardBase/DashboardBase.jsx
+++ src/components/DashboardBase/DashboardBase.jsx
@@ -1,11 +1,60 @@
 // Base dashboard layout (sidebar, navbar, empty content, etc.)
 
 import React from 'react';
+import {
+    Layout,
+    Menu,
+    Icon
+} from 'antd';
 
-import styles from '_DashboardBase.module.scss';
+import 'antd/dist/antd.css';
+import styles from './DashboardBase.module.scss';
 
-const DashboardBase = () => (
-    <h1>hi</h1>
-);
 
-export default DashboardBase;
+const { Header, Sider, Content } = Layout;
+
+class SiderDemo extends React.Component {
+    state = {
+        collapsed: false,
+    };
+
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    };
+
+    render() {
+        return (
+            <Layout>
+                <Sider
+                    trigger={null}
+                    collapsible
+                    collapsed={this.state.collapsed}>
+                    <div className={styles.logo} />
+                    <Menu
+                        theme="dark"
+                        mode="inline"
+                        defaultSelectedKeys={'1'}>
+                        <Menu.Item key="1">
+                            <Icon type="user" />
+                            <span>nav 1</span>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout>
+                    <Header
+                        style={{background: '#fff', padding: 0}}>
+                            <Icon className={styles.trigger} type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} onClick={this.toggle}/>
+                    </Header>
+                    <Content
+                        style={{margin: '24px 16px', padding: 24, background: '#fff', minHeight: 200}}>
+                        Content
+                    </Content>
+                </Layout>
+            </Layout>
+        )
+    }
+}
+
+export default SiderDemo;
