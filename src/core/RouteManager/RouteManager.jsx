4bb5df0f1b321029c6e81e2933f6fd9a5e00de9f
Ying Wang <yingw787.personal@gmail.com>
2020-02-21 15:28:08 -0500

    Update RouteManager.jsx to view all existing pages, and SignupForm.module.scss update styling.

--- src/core/RouteManager/RouteManager.jsx
+++ src/core/RouteManager/RouteManager.jsx
@@ -15,14 +15,14 @@ import {
 } from 'react-router-dom';
 
 import DashboardBase from 'components/DashboardBase/DashboardBase.jsx';
-import SignupForm from 'components/SignupForm/SignupForm.jsx';
 import _401 from 'pages/401/_401.jsx';
 import _404 from 'pages/404/_404.jsx';
+import Signup from 'pages/Signup/Signup.jsx';
 
 
 const RouteManager = () => (
     <Switch>
-        <Route exact path='/' component={SignupForm} />
+        <Route exact path='/' component={Signup} />
         <Route exact path='/dashboard' component={DashboardBase} />
         <Route exact path='/401' component={_401} />
         <Route exact path='/404' component={_404} />
