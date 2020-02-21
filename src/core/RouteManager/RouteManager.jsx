92c98830a79aaa8d910cb0b9dd39a098fca6305b
Ying Wang <yingw787.personal@gmail.com>
2020-02-21 14:51:40 -0500

    Update RouteManager.jsx with views for all existing components.

--- src/core/RouteManager/RouteManager.jsx
+++ src/core/RouteManager/RouteManager.jsx
@@ -14,11 +14,18 @@ import {
     Route
 } from 'react-router-dom';
 
+import DashboardBase from 'components/DashboardBase/DashboardBase.jsx';
 import SignupForm from 'components/SignupForm/SignupForm.jsx';
+import _401 from 'pages/401/_401.jsx';
+import _404 from 'pages/404/_404.jsx';
+
 
 const RouteManager = () => (
     <Switch>
         <Route exact path='/' component={SignupForm} />
+        <Route exact path='/dashboard' component={DashboardBase} />
+        <Route exact path='/401' component={_401} />
+        <Route exact path='/404' component={_404} />
     </Switch>
 );
 
