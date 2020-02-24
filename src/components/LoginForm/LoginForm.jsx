1829c97c613d6cea71ad88446f945f58d6f5f8d3
Ying Wang <yingw787.personal@gmail.com>
2020-02-24 13:26:12 -0500

    Update LoginForm.jsx.

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,3 +1,27 @@
 // Card with form to enable human user-agent to log into TinyDevCRM.
 
 import React from 'react';
+import {
+    Button
+} from 'antd';
+import classnames from 'classnames';
+
+import 'antd/dist/antd.css';
+import styles from './LoginForm.module.scss';
+
+
+const passwordStyling = classnames(
+    styles.formInput,
+    styles.formPasswordInput
+)
+
+class LoginForm extends React.Component {
+    render() {
+        return (
+            <div>hi</div>
+        )
+    }
+}
+
+
+export default LoginForm;
