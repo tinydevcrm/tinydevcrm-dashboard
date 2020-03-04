4ec36e6d59f755f105b0377e2aa3f9c0ba69498f
Ying Wang <yingw787.personal@gmail.com>
2020-03-04 18:08:46 -0500

    Update ProtectedResourceTest.jsx naive protection, access token expired.

--- src/components/ProtectedResourceTest/ProtectedResourceTest.jsx
+++ src/components/ProtectedResourceTest/ProtectedResourceTest.jsx
@@ -7,10 +7,39 @@ import DashboardBase from 'components/DashboardBase/DashboardBase.jsx';
 
 
 class ProtectedResourceTest extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            message: ''
+        };
+
+        this.getMessage = this.getMessage.bind(this);
+    }
+
+    async getMessage() {
+        try {
+            const response = await axiosInstance.get('/hello/');
+            const message = response.data.hello;
+            this.setState({
+                message: message,
+            });
+            return response.data;
+        } catch(error) {
+            console.log('Error: ', JSON.stringify(error, null, 4));
+            throw error;
+        }
+    }
+
+    componentDidMount() {
+        const messageData1 = this.getMessage();
+        console.log('messageData1: ', JSON.stringify(messageData1, null, 4));
+    }
+
     render() {
         return (
             <DashboardBase>
-                <h1>hello</h1>
+                <h1>{this.state.message}</h1>
             </DashboardBase>
         )
     }
