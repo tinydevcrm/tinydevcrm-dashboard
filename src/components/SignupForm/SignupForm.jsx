70ce574506766721a93dba9310e9fd7eef915f27
Ying Wang <yingw787.personal@gmail.com>
2020-03-04 15:48:00 -0500

    Update SignupForm.jsx update form with state variables.

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -16,9 +16,34 @@ const passwordStyling = classnames(
 )
 
 class SignupForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            fullName: "",
+            password: "",
+            passwordConfirm: ""
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <form className={styles.container}>
+            <form
+                className={styles.container}
+                onSubmit={this.handleSubmit}>
                 <label className={styles.formTitle}>
                     Sign Up
                 </label>
@@ -26,22 +51,42 @@ class SignupForm extends React.Component {
                 <label className={styles.formInputLabel}>
                     Email
                 </label>
-                <input type="email" className={styles.formInput} />
+                <input
+                    name="email"
+                    type="email"
+                    className={styles.formInput}
+                    value={this.state.email}
+                    onChange={this.handleChange} />
 
                 <label className={styles.formInputLabel}>
                     Full Name
                 </label>
-                <input type="text" className={styles.formInput} />
+                <input
+                    name="fullName"
+                    type="text"
+                    className={styles.formInput}
+                    value={this.state.fullName}
+                    onChange={this.handleChange} />
 
                 <label className={styles.formInputLabel}>
                     Password
                 </label>
-                <input type="password" className={passwordStyling} />
+                <input
+                    name="password"
+                    type="password"
+                    className={passwordStyling}
+                    value={this.state.password}
+                    onChange={this.handleChange} />
 
                 <label className={styles.formInputLabel}>
                     Confirm Password
                 </label>
-                <input type="password" className={passwordStyling} />
+                <input
+                    name="passwordConfirm"
+                    type="password"
+                    className={passwordStyling}
+                    value={this.state.passwordConfirm}
+                    onChange={this.handleChange} />
 
                 <div className={styles.centered}>
                     <Button
