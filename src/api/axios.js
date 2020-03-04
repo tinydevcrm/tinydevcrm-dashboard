a3f531fe18ffcf85edf3f7adc2752d5982487a9d
Ying Wang <yingw787.personal@gmail.com>
2020-03-04 18:30:48 -0500

    Update axios.js add axios interceptor to refresh the access token if a refresh token is available.

--- src/api/axios.js
+++ src/api/axios.js
@@ -14,5 +14,37 @@ const axiosInstance = axios.create({
         'accept': 'application/json'
     }
 });
+// Handle JWT access token auto-refresh based on liveness of JWT refresh token.
+// I'm guessing that it intercepts existing requests, and if they fail due to
+// 401 unauthorized
+axiosInstance.interceptors.response.use(
+    response => response,
+    error => {
+        const originalRequest = error.config;
+        if (
+            error.response.status === 401 &&
+            error.response.statusText === 'Unauthorized'
+        ) {
+            const refresh_token = localStorage.getItem('refresh_token');
+            return axiosInstance.post(
+                '/token/refresh/',
+                {
+                    refresh: refresh_token
+                }
+            ).then((response) => {
+                localStorage.setItem('access_token', response.data.access);
+                localStorage.setItem('refresh_token', response.data.refresh);
+
+                axiosInstance.defaults.headers['Authorization'] = 'JWT ' + response.data.access;
+                originalRequest.headers['Authorization'] = 'JWT ' + response.data.access;
+
+                return axiosInstance(originalRequest);
+            }).catch(err => {
+                console.log(err)
+            });
+        }
+        return Promise.reject(error);
+    }
+)
 
 export default axiosInstance;
