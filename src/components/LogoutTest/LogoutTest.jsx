4276e43e04e90df91829193d898030ef8c82b9df
Ying Wang <yingw787.personal@gmail.com>
2020-03-05 15:53:30 -0500

    Update LogoutTest.jsx adding working handleLogout method.

--- src/components/LogoutTest/LogoutTest.jsx
+++ src/components/LogoutTest/LogoutTest.jsx
@@ -16,8 +16,21 @@ class LogoutTest extends React.Component {
         this.handleLogout = this.handleLogout.bind(this);
     }
 
-    handleLogout() {
-        console.log('hi');
+    async handleLogout() {
+        try {
+            const response = await axiosInstance.post(
+                '/blacklist/',
+                {
+                    'refresh_token': localStorage.getItem('refresh_token')
+                }
+            );
+            localStorage.removeItem('access_token');
+            localStorage.removeItem('refresh_token');
+            axiosInstance.defaults.headers['Authorization'] = null;
+            return response
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     render() {
